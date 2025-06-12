81a971351a350abb0c324a6301e2c03dd6b12a84
linkotylar <linko96.ko@gmail.com>
2025-06-12 14:54:19 +0700

    update error

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Padauk } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const padauk = Padauk({ 
+    weight: ['400', '700'],
+    subsets: ['latin']
+});
